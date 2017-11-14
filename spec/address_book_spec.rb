4c282976acb1c6458428090a81d97ea205916b00
Colin <marshallcolin71@gmail.com>
2017-11-13 20:46:42 -0500

    checkpoint 6 assignment complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -99,4 +99,11 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "importing from entries_2.csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      expect(book.entries.size).to eq 3
+    end
+  end
+
 end
